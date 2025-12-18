d14a242125315ab271b821c9d1e14628d2275dc3
D-avlessi-webcoom <d.avlessi@webcoompartnership.net>
2025-12-18 17:08:59 +0100

    Content hashing

--- vite.config.ts
+++ vite.config.ts
@@ -39,6 +39,15 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+      build: {
+        rollupOptions: {
+          output: {
+            entryFileNames: 'assets/[name].[hash].js',
+            chunkFileNames: 'assets/[name].[hash].js',
+            assetFileNames: 'assets/[name].[hash].[ext]'
+          }
+        }
+      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
